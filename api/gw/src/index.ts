64026cb5f455df594f02d08d751780b7301885bc
bmarcella <bmarcella@gmail.com>
2024-07-01 23:20:58 -0400

    Removed accidental submodule

---
+++ api/gw/src/index.ts
@@ -0,0 +1,18 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { Eureka } from 'eureka-js-client';
+import express, { Express } from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+import {
+  EurekaTools,
+  setProxies,
+} from '../../../common/eureka/Eureka';
+import { services } from '../../../common/index/services';
+
+dotenv.config();
+const ET = EurekaTools<Eureka>(Eureka, process.env);
+setProxies<Express, Eureka>(process.env, ET, express, cors, ET.EurekaClient, services, createProxyMiddleware);
+
+
+
