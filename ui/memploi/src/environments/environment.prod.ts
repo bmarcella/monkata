463e66331ecc160fe530e54c00eeb2e2bc9774b0
bmarcella <bmarcella@gmail.com>
2024-07-18 00:07:10 -0400

    update cv

--- ui/memploi/src/environments/environment.prod.ts
+++ ui/memploi/src/environments/environment.prod.ts
@@ -27,7 +27,7 @@ export const environment = {
   recaptcha: {
     siteKey: ReCaptchaPublicKey ,
   },
-  version: "1.0.50"
+  version: "1.0.60"
 };
 export const getURL = (service: string, endpoint: string) => {
   return `${environment.gateway}${environment.services[service].path}/${endpoint}`;
