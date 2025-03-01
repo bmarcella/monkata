d213725ea0336964223852fef0b2ecc7c3293030
bmarcella <bmarcella@gmail.com>
2025-03-01 13:34:38 -0500

    delete DOM

--- common/DynamicObject/projects/dom/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of dom
- */
-
-export * from './lib/components/dom/dom.service';
-export * from './lib/components/dom/dom.component';
-export * from './lib/components/dom/dom.module';
